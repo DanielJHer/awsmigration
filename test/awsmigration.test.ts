a6ae993748e5e713330ee03e7a14a954b918b820
Daniel Her <danieljhher@gmail.com>
2024-12-01 12:36:03 -0800

    added cdk

---
+++ test/awsmigration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Awsmigration from '../lib/awsmigration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awsmigration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Awsmigration.AwsmigrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
